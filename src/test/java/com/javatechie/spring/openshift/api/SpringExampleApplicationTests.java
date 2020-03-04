e525378252c9892a11ae312488888247cd1328a1
ludwing979 <magoludwing@gmail.com>
2020-03-04 14:43:45 -0600

    Ultimos arreglos

--- src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
+++ src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
@@ -1,13 +1,16 @@
 package com.javatechie.spring.openshift.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringExampleApplicationTests {
+public class SpringExampleApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
