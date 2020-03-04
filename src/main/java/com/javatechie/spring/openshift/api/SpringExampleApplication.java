d2678f950fe12ce3ec7dc358de6cd840cbe25c7f
ludwing979 <magoludwing@gmail.com>
2020-03-04 13:46:50 -0600

    Se configuro correctamente las carpetas para el despliegue en openshift

--- src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
+++ src/main/java/com/javatechie/spring/openshift/api/SpringExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.springexample;
+package com.javatechie.spring.openshift.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
+++ src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.springexample;
+package com.javatechie.spring.openshift.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
