800b91321e7a30ebbc877029125b21c0745ca7c3
ludwing979 <magoludwing@gmail.com>
2020-03-05 10:38:28 -0600

    Se arregla problema con maven

--- src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
+++ src/test/java/com/javatechie/spring/openshift/api/SpringExampleApplicationTests.java
@@ -2,10 +2,12 @@ package com.javatechie.spring.openshift.api;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@SpringBootApplication
 @SpringBootTest
 public class SpringExampleApplicationTests {
 
